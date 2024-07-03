71c8b29cf63467a018c2ee8503abdf4ff036bc77
Mathias Nonohay <mathiasnonohay@icloud.com>
2024-07-03 13:36:41 -0300

    Added register cells to main tableView

--- FeaturedMovie/Controllers/MainViewController/MainViewController.swift
+++ FeaturedMovie/Controllers/MainViewController/MainViewController.swift
@@ -12,6 +12,8 @@ class MainViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet weak var tableView: UITableView!
     
+    var viewModel: MainViewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,13 +35,27 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
         self.tableView.dataSource = self
         
         self.tableView.backgroundColor = .clear
+        
+        self.registerCells()
+    }
+    
+    private func registerCells() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        viewModel.numberOfSections()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        viewModel.numberOfRows(in: section)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Cell at row: \(indexPath.row)"
+        
+        return cell
     }
 }
---
+++ FeaturedMovie/ViewModel/MainViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewModel.swift
+//  FeaturedMovie
+//
+//  Created by Mathias Nonohay on 03/07/24.
+//
+
+import UIKit
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        return 1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        return 5
+    }
+}
