93b4de1425104371738962877176a5c123f374a4
Mathias Nonohay <mathiasnonohay@icloud.com>
2024-07-03 13:06:02 -0300

    Add NavigationController and title to MainViewController

--- FeaturedMovie/AppDelegate.swift
+++ FeaturedMovie/AppDelegate.swift
@@ -10,10 +10,18 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
         return true
     }
 
