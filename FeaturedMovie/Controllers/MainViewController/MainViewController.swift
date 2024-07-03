2fb103291fb6b007ff215fe2c96ecad30bcc10b2
Mathias Nonohay <mathiasnonohay@icloud.com>
2024-07-03 13:29:32 -0300

    Added tableView that will contain all Movie cells

--- FeaturedMovie/Controllers/MainViewController/MainViewController.swift
+++ FeaturedMovie/Controllers/MainViewController/MainViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.configureView()
+    }
+    
+    private func configureView() {
         self.title = "Movies"
+        
+        self.configureTableView()
+    }
+}
+
+// MARK: - UITableView
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    private func configureTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.backgroundColor = .clear
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
